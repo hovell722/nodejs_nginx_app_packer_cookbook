5f1920d9bdc05ebe673c9d65af0409404605221b
James Hovell <j.hovell722@gmail.com>
2020-04-09 17:56:58 +0100

    berksfile

---
+++ cookbooks/nodejs_nginx/README.md
@@ -0,0 +1,44 @@
+# Nodejs and Nginx Cookbook Eng54 :penguin:
+
+This cookbook will install nodejs and nginx.
+
+To use it import it in your own cookbook and run it in the recipies.
+
+## Learning Outcomes:
+- Git, Github
+- Documentation
+- Chef, Inspec, Chefspec
+- Importing Cookbooks (will require our search)
+- Attributes, and Templates
+- TDD
+
+
+## Installation
+
+## Tests
+
+To run the unit tests run:
+
+```
+  $ chef exec rspec
+```
+
+To run integration test:
+
+```
+  $ kitchen test
+```
+
+## ERB and ice cream cones <% %> and <%= %>
+
+ERB is embedded ruby.
+
+Meaning you can interpolate variables or run ruby code.
+
+It can then be converted to a string.
+
+ERB is like the f'this is python {hello} ' but for entire documents!
+
+#### <% %> allow ruby with no output
+
+#### <%= %> allows ruby with output (I mean like printing to the documents)
---
+++ cookbooks/nodejs_nginx/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook:: nodejs_nginx
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'nginx'
+# package 'nodejs'
+# package 'npm'
+
+
+service 'nginx' do
+  action [:enable, :start]
+end
+
+# Creating template in machine
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']
+  notifies :restart, 'service[nginx]'
+end
+
+# Making symbolic link to above template
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies :restart, 'service[nginx]'
+end
+
+#
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
+
+include_recipe 'nodejs'
+# does not install npm by default
+
+
+npm_package 'pm2'
+npm_package 'react'
